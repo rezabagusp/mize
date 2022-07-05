cf4de148ae5533664d239c1f703dbfac9d028471
rezabagusp <rezabaguspermana.rbp@gmail.com>
2022-07-05 14:55:22 +0700

    feat: SEO

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 
 import AppLayout from '../components/appLayout';
+import SEO from '../components/seo';
 import HomeScreen from '../screens/home';
 
 const Home = () => (
-  <AppLayout>
-    <HomeScreen />
-  </AppLayout>
+  <>
+    <SEO title="Home" />
+    <AppLayout>
+      <HomeScreen />
+    </AppLayout>
+  </>
 );
 
 export default Home;
