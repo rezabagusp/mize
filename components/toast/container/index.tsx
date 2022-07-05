60d47ddb7b724d5a2e4c7e5ee5e31ae88a796ce3
Reza Bagus Permana <rezabaguspermana.rbp@gmail.com>
2022-07-05 15:20:57 +0700

    refactor: remove unnecessary things (#6)

--- components/toast/container/index.tsx
+++
@@ -1,6 +0,0 @@
-import React, { ReactElement } from 'react';
-import { TOAST_WRAPPER_ID } from '../config';
-
-const ToastWrapper = (): ReactElement => <div id={TOAST_WRAPPER_ID} />;
-
-export default ToastWrapper;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,11 @@
 import React, { ReactNode } from 'react';
 import type { AppProps } from 'next/app';
 
-import ToastWrapper from '../components/toast';
 import '../styles/globals.css';
 
 const MyApp = ({
   Component,
   pageProps,
-}: AppProps): ReactNode => (
-  <>
-    <Component {...pageProps} />
-    <ToastWrapper />
-  </>
-);
+}: AppProps): ReactNode => <Component {...pageProps} />;
 
 export default MyApp;
